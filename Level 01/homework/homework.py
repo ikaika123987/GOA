9cc5e0aed6afd2418b523925e751234f91fc0afa
ikaika123987 <irakliozashvili69@gmail.com>
2024-09-11 23:26:31 +0400

    GOA

---
+++ Level 01/homework/homework.py
@@ -0,0 +1,6 @@
+name = "irakli"
+surname = "ozashvili"
+age = 15
+height = 1.75
+
+print(name + " " + surname + " " + str(age) + " " + str(height))
---
+++ Level 02/homework/homework.py
@@ -0,0 +1,29 @@
+name = "irakli"
+surname = "ozashvili"
+age = 15
+height = 1.75
+
+print(name + " " + surname + " " + str(age) + " " + str(height))
+
+
+favorite_food = "pizza"
+favorite_color = "white"
+print("my favorite food is", favorite_food)
+print("my favorite color is", favorite_color)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
