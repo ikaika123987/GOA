081f1dabc4fab8fe7f6e83a81bac70b2c66d909d
ikaika123987 <irakliozashvili69@gmail.com>
2024-09-16 14:08:11 +0400

    GOA

---
+++ Level03/homework/homework.py
@@ -0,0 +1,20 @@
+country = "georgia"
+name = "irakli"
+surname = "ozashvili"
+age = 15
+height = 1.75
+job = "developer"
+hobby = "watching movies"
+city = "tbilisi"
+favorite_food = "pizza" 
+favorite_color = "white"
+
+sentence = f"my name is {name} {surname}, i'm from {country}, i'm {age} years old,i'm {height} i'm in {city}, my favourite food is {favorite_food}, my favourite color is {favorite_color}, i'm {job}, my hobby is {hobby} "
+
+print(sentence)
+
+
+
+
+
+
---
+++ Level03/homework/homework02.py
@@ -0,0 +1,18 @@
+country = "georgia"
+name = "irakli"
+surname = "ozashvili"
+age = 15
+hobby = "watching movies"
+
+print(country,name,surname,age,hobby)
+
+
+
+
+
+
+
+
+
+
+
