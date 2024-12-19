2d833895a74fe40fea69328bddaf48a23cf99911
ikaika123987 <irakliozashvili69@gmail.com>
2024-12-19 21:35:29 +0400

    GOA

--- Level 03/classwork/classwork.py
+++ Level 03/classwork/classwork.py
@@ -1,1 +1,13 @@
-print("no classwork")
+#level 3: 1) შექმენით ცვლადი რომელსაც დააქრმევთ name და ასევე შექმნით ცვლადს last_name მათ დავამახასოვრებინებთ ჩევნეს სახლებეს და გვარებს საბოლოოდ კი ერთად დავპირნტავთ
+name = "irakli"
+last_name = "ozashvili"
+
+print(name, last_name)
+
+
+
+
+
+
+
+
---
+++ Level 03/classwork/classwork02.py
@@ -0,0 +1,8 @@
+#2) შედქმენით ცვლადი რომელსაც დაარქმევთ num1 და num2 მათ შეანახინეთ ერთს int data type მეროეს float data type
+
+num1 = 5
+num2 = 1.5
+
+print(num1, num2)
+print(type(num1))
+print(type(num2))
